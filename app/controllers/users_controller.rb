c0cd0046d9fb96186c00db1370f27cbe7e7a3024
Dian Pan & Mark Miller <pair+dianpan.markmiller21@devbootcamp.com>
2015-09-28 12:28:40 -0400

    add user_params method, allow for user registration, add flash error for users that already exist

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,29 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def show
-    @user = User.find(params[:id])
+  def new
+    @user = User.new
   end
+
+  def show
+    @user = User.find(user_params[:id])
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+      flash[:dup_user] = "You already exist, please login"
+      redirect_to '/login'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :username, :password)
+  end
+
 end
