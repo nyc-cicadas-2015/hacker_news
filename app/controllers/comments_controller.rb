f1c6c912d9bcd469ba34f1fe088158bd488bf5bb
Dian Pan & Mark Miller <pair+dianpan.markmiller21@devbootcamp.com>
2015-09-28 17:11:55 -0400

    add commenting functionality

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,18 @@
 class CommentsController < ApplicationController
+  def create
+    post = Post.find(params[:comment][:post_id])
+    comment = post.comments.build(comment_params)
+    if comment.save
+      comment.update_attributes(user_id: session[:user_id])
+    else
+      flash[:invalid_comment] = "missing field in comment forms, try again"
+    end
+    redirect_to "/posts/#{post.id}"
+  end
 
+
+  private
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
 end
