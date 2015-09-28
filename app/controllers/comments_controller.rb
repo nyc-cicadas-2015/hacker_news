1cc1e5a30b4a50cb4a254500c2b456fcebee2995
Dian Pan & Mark Miller <pair+dianpan.markmiller21@devbootcamp.com>
2015-09-28 17:51:09 -0400

    add comment delete functionality

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,9 +10,27 @@ class CommentsController < ApplicationController
     redirect_to "/posts/#{post.id}"
   end
 
+  def edit
+    @comment = Comment.find(params[:id])
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    post = @comment.post
+    @comment.update(comment_params)
+    redirect_to post_path(post.id)
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    post = @comment.post
+    @comment.destroy
+    redirect_to post_path(post.id)
+  end
 
   private
   def comment_params
     params.require(:comment).permit(:body)
   end
 end
+
