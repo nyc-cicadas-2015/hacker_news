fa8c66b6008a9df71276fe848cec87652fb49b3e
Dian Pan & Mark Miller <pair+dianpan.markmiller21@devbootcamp.com>
2015-09-28 15:38:43 -0400

    add post creation and validations

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,27 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    user = User.find_by(id: session[:user_id])
+    post = user.posts.build(post_params)
+    if post.save
+          binding.pry
+      redirect_to '/'
+    else
+      flash[:error] = "post did not save, please try again."
+      redirect_to '/posts/new'
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:url, :title)
+  end
+
 end
+
