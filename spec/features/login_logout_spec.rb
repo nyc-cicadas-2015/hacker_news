1efe0285e3409e51a03834f28fcbb352061ab13f
Dian Pan & Mark Miller <pair+dianpan.markmiller21@devbootcamp.com>
2015-09-29 17:05:39 -0400

    move helper logic in login_logout_spec

--- spec/features/login_logout_spec.rb
+++ spec/features/login_logout_spec.rb
@@ -2,13 +2,9 @@ require_relative '../rails_helper'
 
 describe "the signin process" do
   let(:log_me_in) {
-    visit login_path
-    within("#new_user") do
-      fill_in 'user_username', :with => user[:username]
-      fill_in 'user_password', :with => user[:password]
-    end
-    click_button 'Login'
+    LoginPageHelper.new.visit_page.login(user)
   }
+
 
   before :each do
     create(:user)
