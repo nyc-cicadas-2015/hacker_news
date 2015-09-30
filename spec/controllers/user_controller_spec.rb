3c374444a86637579def3392b1f68dd14994f7d0
Dian Pan & Mark Miller <pair+dianpan.markmiller21@devbootcamp.com>
2015-09-30 10:49:17 -0400

    remove index user routes and build new route controller test

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
-describe UserController do
-
+describe UsersController do
+  describe "Get new" do
+    it "Creates a new user" do
+      get :new
+      expect(assigns(:user)).to be_a(User)
+    end
+  end
 end
